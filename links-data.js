13adb0ed0d7b653616f8acdc6d41db5caa27fb70
nicolabraga <nicola.emfocosurf@gmail.com>
2026-08-22 19:07:29 -0300

    Auto-update: +1 novos

--- links-data.js
+++ links-data.js
@@ -1,9 +1,13 @@
 // Gerado automaticamente por update_links.py
 // Fonte: https://fotopix.com.br/
-// Atualizado em: 22/08/2026 18:31
+// Atualizado em: 22/08/2026 19:07
 window.LINKS_DATA = {
-  "updated_at": "22/08/2026 18:31",
+  "updated_at": "22/08/2026 19:07",
   "links": [
+    {
+        "href": "https://fotopix.com.br/album/25e61f",
+        "text": "Beira Mar - Tarde/Noite - Fortaleza - 22/08/2026 - Álbum de Fotos de Corrida"
+    },
     {
         "href": "https://fotopix.com.br/album/25e61d",
         "text": "Beira Mar - Fortaleza - 22/08/2026 - Álbum de Fotos de Corrida"
