e0b7636443b69a8a154093a1ce824518ce290941
nicolabraga <nicola.emfocosurf@gmail.com>
2026-08-30 11:42:39 -0300

    Auto-update: +3 novos

--- links-data.js
+++ links-data.js
@@ -1,9 +1,21 @@
 // Gerado automaticamente por update_links.py
 // Fonte: https://fotopix.com.br/
-// Atualizado em: 29/08/2026 09:07
+// Atualizado em: 30/08/2026 11:42
 window.LINKS_DATA = {
-  "updated_at": "29/08/2026 09:07",
+  "updated_at": "30/08/2026 11:42",
   "links": [
+    {
+        "href": "https://fotopix.com.br/album/265268",
+        "text": "Treino Beira Mar Corrida Manhã - Fortaleza - 30/08/2026 - Álbum de Fotos de Corrida"
+    },
+    {
+        "href": "https://fotopix.com.br/album/26526a",
+        "text": "Challenge Brasil Triathlon - 2026 - Fortaleza 30/08/2026 - Álbum de Fotos de Corrida"
+    },
+    {
+        "href": "https://fotopix.com.br/album/265263",
+        "text": "Treino Beira Mar Corrida - Tarde"
+    },
     {
         "href": "https://fotopix.com.br/album/265262",
         "text": "Treino Beira Mar Corrida Manhã - Fortaleza - 29/08/2026 - Álbum de Fotos de Corrida"
