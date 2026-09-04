c111c7246cb7dbb06060bd4883eec2534fecac57
nicolabraga <nicola.emfocosurf@gmail.com>
2026-09-04 07:49:57 -0300

    Auto-update: +1 novos

--- links-data.js
+++ links-data.js
@@ -1,9 +1,13 @@
 // Gerado automaticamente por update_links.py
 // Fonte: https://fotopix.com.br/
-// Atualizado em: 03/09/2026 19:07
+// Atualizado em: 04/09/2026 07:49
 window.LINKS_DATA = {
-  "updated_at": "03/09/2026 19:07",
+  "updated_at": "04/09/2026 07:49",
   "links": [
+    {
+        "href": "https://fotopix.com.br/album/26b40d",
+        "text": "Treino Beira Mar Corrida Manhã - Fortaleza - 04/09/2026 - Álbum de Fotos de Corrida"
+    },
     {
         "href": "https://fotopix.com.br/album/26b407",
         "text": "Treino Beira Mar Corrida Manhã - Fortaleza - 03/09/2026 - Álbum de Fotos de Corrida"
