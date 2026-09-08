f70a7943aefdf216e950fc844f3c3b418bf66f90
nicolabraga <nicola.emfocosurf@gmail.com>
2026-09-08 07:50:19 -0300

    Auto-update: +2 novos

--- links-data.js
+++ links-data.js
@@ -1,9 +1,17 @@
 // Gerado automaticamente por update_links.py
 // Fonte: https://fotopix.com.br/
-// Atualizado em: 07/09/2026 19:10
+// Atualizado em: 08/09/2026 07:50
 window.LINKS_DATA = {
-  "updated_at": "07/09/2026 19:10",
+  "updated_at": "08/09/2026 07:50",
   "links": [
+    {
+        "href": "https://fotopix.com.br/album/2705f3",
+        "text": "Treino Beira Mar Corrida Manhã - Fortaleza - 08/09/2026 - Álbum de Fotos de Corrida"
+    },
+    {
+        "href": "https://fotopix.com.br/album/2705ed",
+        "text": "Treino Sabiaguaba Ciclismo CE010 - Fortaleza - 08/09/2026 - Álbum de Fotos de Ciclismo"
+    },
     {
         "href": "https://fotopix.com.br/album/2705b6",
         "text": "Treino Beira Mar Corrida Tarde"
