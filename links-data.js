fa60f5392dcb736d40045776cdc2923962286fa9
nicolabraga <nicola.emfocosurf@gmail.com>
2026-09-20 18:35:03 -0300

    Auto-update: +1 novos

--- links-data.js
+++ links-data.js
@@ -1,9 +1,13 @@
 // Gerado automaticamente por update_links.py
 // Fonte: https://fotopix.com.br/
-// Atualizado em: 20/09/2026 08:38
+// Atualizado em: 20/09/2026 18:34
 window.LINKS_DATA = {
-  "updated_at": "20/09/2026 08:38",
+  "updated_at": "20/09/2026 18:34",
   "links": [
+    {
+        "href": "https://fotopix.com.br/album/278cad",
+        "text": "Treino Beira Mar Corrida Tarde"
+    },
     {
         "href": "https://fotopix.com.br/album/278cc2",
         "text": "SANTANDER TRACK&amp;FIELD RUN SERIES - SHOPPING DEL PASEO IICorrida - 2026 - Fortaleza 20/09/2026 - Álbum de Fotos de Corrida"
